89f8f0499c7f97264f5e4a04029c367c39cddefc
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-01-05 15:20:13 +0100

    querying tess

---
+++ js/tess.api.js
@@ -0,0 +1,56 @@
+function tess_api(){
+    function api(){}
+
+    // generic getter
+    api.get_for=function (branch, name){
+        //getter object
+        var getter = function(){}
+        //function to count the number of tools associated
+        getter.count=function(callback){
+            return $.ajax({
+                url:getter.get_api_url(),
+                type: "GET",
+                dataType: "json",
+                data: {},
+                success: function(data, statut){
+                    callback(data.length,data,statut);
+                },
+                error:function (textStatus, xhr) {
+                    console.error(textStatus);
+                    console.error(xhr);
+                    callback(-1,[],textStatus);
+                },
+            });
+        }
+        //is the count function enabled
+        getter.is_enabled=function(){
+            return true;
+        }
+        //get the url returning the tools for human
+        getter.get_url=function(){
+            return "https://tess.elixir-europe.org/materials?scientific_topics="+name.replace(/ /g,'+');
+        }
+        //get the url returning the tools for api call
+        getter.get_api_url=function(){
+            return "https://tess.elixir-europe.org/materials.json?scientific_topics="+name.replace(/ /g,'%20');
+        }
+        return getter;
+    }
+    //getter for topics
+    api.get_for_topic=function (name){
+        return api.get_for("topic",name)
+    }
+    //getter for operations
+    api.get_for_operation=function (name){
+        return api.get_for("operation",name)
+    }
+    //getter for data
+    api.get_for_data=function (name){
+        return api.get_for("data",name)
+    }
+    //getter for format
+    api.get_for_format=function (name){
+        return api.get_for("format",name)
+    }
+    return api;
+}
