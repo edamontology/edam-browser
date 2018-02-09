8fe373be6947259e5fb58ed31898df888af3ee4b
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-02-09 13:15:22 +0100

    Add link to WebProtégé lastest stable version

--- js/tree-edam-stand-alone.js
+++ js/tree-edam-stand-alone.js
@@ -281,8 +281,10 @@ function interactive_edam_browser(){
             "<a target=\"_blank\" href=\"http://aber-owl.net/ontology/EDAM/#/Browse/%3Chttp%3A%2F%2Fedamontology.org%2F"+identifier+"%3E\">AberOWL</a>"+
             ", "+
             "<a target=\"_blank\" href=\"http://bioportal.bioontology.org/ontologies/EDAM/?p=classes&conceptid=http%3A%2F%2Fedamontology.org%2F"+identifier+"\">BioPortal</a>"+
-            " or "+
+            ", "+
             "<a target=\"_blank\" href=\"https://www.ebi.ac.uk/ols/ontologies/edam/terms?iri=http%3A%2F%2Fedamontology.org%2F"+identifier+"\">OLS</a>"+
+            " or "+
+            "<a target=\"_blank\" href=\"https://webprotege.stanford.edu/#projects/98640503-a37d-4404-84da-caf30fadd685/edit/Classes?selection=Class(%3Chttp://edamontology.org/"+identifier+"%3E)\">WebProt&eacuteg&eacute</a>"+
             "."
             );
         }
