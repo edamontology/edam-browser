6ec5b20623f821da5ecb14410eb6e1f4b3b83e1f
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-06-18 13:58:14 +0200

    improve update procedure

--- media/README.md
+++ media/README.md
@@ -1,7 +1,11 @@
 ```
 virtualenv .venv -p python3
+source .venv/bin/activate
 pip install git+https://github.com/edamontology/edam2json.git
-wget http://edamontology.org/EDAM.owl
-edam2json EDAM.owl biotools  --extended > edam_extended.biotools.json  
+wget http://edamontology.org/EDAM.owl -O EDAM.owl
+edam2json EDAM.owl biotools  --extended > edam_extended.biotools.json
 ./min_json.py 
+export VERSION=$(python -c 'import json; print(json.load(open("edam_extended.biotools.min.json"))["meta"]["version"])')
+mv EDAM.owl  EDAM.$VERSION.owl 
+cp edam_extended.biotools.json edam_extended_$VERSION.biotools.json
 ```
--- media/min_json.py
+++ media/min_json.py
@@ -2,9 +2,9 @@
 import json
 import os
 
-for _, _, filenames in os.walk('.'):
-    for filename in filenames:
-        if filename.endswith(".json") and not filename.endswith("min.json"):
-            data = json.load(open(filename))
-            with open(filename[:-5] + '.min.json', 'w') as outfile:
-                json.dump(data, outfile)
+for filename in os.listdir('.'):
+    print(filename)
+    if filename.endswith(".json") and not filename.endswith("min.json"):
+        data = json.load(open(filename))
+        with open(filename[:-5] + '.min.json', 'w') as outfile:
+            json.dump(data, outfile)
