287b4cdefb610ae48f62d4e3a70bfbb27283c196
HagerDakroury <hagerdakroury@gmail.com>
2021-03-30 09:09:07 +0200

    fixed duplicate parents bug

--- js/tree-edam-stand-alone.js
+++ js/tree-edam-stand-alone.js
@@ -428,6 +428,9 @@ function interactive_edam_browser(){
         name=name.charAt(0).toUpperCase()+name.substring(1);
         if (value.constructor === Array){
             if (value.length>1){
+                //removing duplicates
+                var uniqueValues=[...new Set(value)];
+                value=uniqueValues; 
                 value_txt="";
                 for (i=0; i<value.length;i++){
                     if (value[i] != ""){
