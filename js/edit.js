cb48519e2953caaf8f27a713fb6eb0fb52904ddb
pooja2299 <pyadav2299@gmail.com>
2021-05-10 15:51:46 +0200

    moving show/hide of per-branch element inside build_form

--- js/edit.js
+++ js/edit.js
@@ -67,6 +67,10 @@ function build_form(target, parentOfNewTerm){
     }else{
         $(".search-term").val(parentOfNewTerm.text);
     }
+    var s_branch = getUrlParameter('term') || getUrlParameter('parent');
+    s_branch = s_branch.substring(s_branch.lastIndexOf('/') + 1, s_branch.lastIndexOf('_'));
+    $('.optional_rel').hide();
+    $('.' + s_branch + '_rel').show();
     $("form [disabled=disabled]").attr("disabled", false);
 }
 function join_if_exists(tab){
@@ -114,10 +118,6 @@ window.onload = function() {
         fill_form(uri, getUrlParameter('parent'), branch);
     });
     browser.current_branch( getUrlParameter('branch'));
-    var s_branch = getUrlParameter('term') || getUrlParameter('parent');
-    s_branch = s_branch.substring(s_branch.lastIndexOf('/') + 1, s_branch.lastIndexOf('_'));
-    $('.optional_rel').hide();
-    $('.' + s_branch + '_rel').show();
 };
 
 function sendToGitHub(){
