97de559807f794d7a52526f5166b735afbc0fc0b
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2021-06-14 15:02:41 +0200

    improve tooltips setting

--- js/tree-edam-stand-alone.js
+++ js/tree-edam-stand-alone.js
@@ -508,8 +508,7 @@ function interactive_edam_browser(){
 
     function tooltipBuilder(d, tooltipContainer){
         tooltipContainer.node().innerHTML = '';
-        if ($("#tree-and-controls:not(:fullscreen)").length &&
-            $("input[name='show-tooltip-only-full-screen']:checked").length)
+        if ($("#tree-and-controls:not(:fullscreen)").length && ! $("input[name='always-show-tooltip']:checked").length)
             return;
         var uri=__my_interactive_tree.identifierAccessor()(d);
         var branch_of_term = get_branch_of_term(uri);
